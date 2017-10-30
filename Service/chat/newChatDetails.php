ce401ffdd450d7009c2af5496b1bf70b2ea18fe4
shashank <shashanksmf@outlook.com>
2017-10-30 03:59:01 -0700

    Update newChatDetails.php
    CORS added

--- Service/chat/newChatDetails.php
+++ Service/chat/newChatDetails.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
 require_once("../../Controller/StaticDBCon.php");
 $conn = new mysqli(StaticDBCon::$servername, StaticDBCon::$username, StaticDBCon::$password, StaticDBCon::$dbname);
 if (!$conn) {
