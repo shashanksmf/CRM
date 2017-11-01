d4d7653290bba2517753b6154feb90924a1436b1
shashank <shashanksmf@outlook.com>
2017-11-01 14:55:45 +0530

    Infobip API updated

--- controllers/mail.js
+++ controllers/mail.js
@@ -50,10 +50,11 @@ inspinia.controller('mailCtrl', ['$scope','$rootScope','$http','$q','API','$stat
 				}
 			//	console.log(phNoArr);
 				
-				var encoded = "VVBTQUlMMTpVMjQyODk3bA==";
+				//var encoded = "VVBTQUlMMTpVMjQyODk3bA==";
+				var encoded = "VVBTQUlsMTpVMjQyODk3bA==";
 				$.ajax({
 	  			type:"POST",
-	  			url:"https://api.infobip.com/sms/1/text/multi",
+	  			url:"https://api.infobip.com/sms/1/text/single",
 	  			headers:{
 	  				"Authorization": "Basic "+encoded,
 	  				"Content-Type":"application/json",
