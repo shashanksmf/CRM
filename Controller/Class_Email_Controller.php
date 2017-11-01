a754b735a82a5f76840e76c9691a01568c82a1d7
shashank <shashanksmf@outlook.com>
2017-11-01 21:22:26 +0530

    Modified : error reporting added

--- Controller/Class_Email_Controller.php
+++ Controller/Class_Email_Controller.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 require_once("../Models/Class_Email.php");
 require_once("../Controller/StaticDBCon.php");
  header("Access-Control-Allow-Origin: *");
--- Service/GetCampiagn.php
+++ Service/GetCampiagn.php
@@ -1,5 +1,6 @@
 <?php 
-	
+error_reporting(E_ALL);
+ini_set('display_errors', '1');	
 	 ob_start();
 	
 	//Link -> localhost/wehnc/Service/GetEmplData.php
