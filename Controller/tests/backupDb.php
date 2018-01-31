3f403ec2664678f771616a16a8c4855cac1952f0
shashankdevasia <shashanksmf@outlook.com>
2018-01-31 12:24:31 +0530

    add db file

---
+++ Controller/tests/backupDb.php
@@ -0,0 +1,26 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+    require_once("./../StaticDBCon.php");
+	
+	$conn = new mysqli(StaticDBCon::$servername, StaticDBCon::$username, StaticDBCon::$password, StaticDBCon::$dbname);
+    $dbhost = StaticDBCon::$servername;
+    $dbuser = StaticDBCon::$username;
+    $dbpass = StaticDBCon::$password;
+    $dbname = StaticDBCon::$dbname;
+
+    if($conn->connect_error) {
+		$responseArr["result"] = false;
+		$responseArr["details"] = $conn->connect_error;
+		exit(json_encode($responseArr));
+	} 
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+    
+   
+  
+   $backup_file = $dbname . date("Y-m-d-H-i-s") . '.gz';
+   $command = "mysqldump --opt -h $dbhost -u $dbuser -p $dbpass ". "$dbname | gzip > $backup_file";
+   
+   system($command);
+?>
