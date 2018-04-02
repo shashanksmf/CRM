57e03d17ecdacae59f0aab3a05ce0aac8ee1d32e
shashank <shashanksmf@outlook.com>
2018-04-03 00:06:49 +0530

    test

--- Controller/tests/mailchimptest/mailChimpUserTest.php
+++ Controller/tests/mailchimptest/mailChimpUserTest.php
@@ -1,26 +1,25 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
+require_once("./../mailChimpConfig.php");
 
 class MailChimpUserTest {
 
 	public function subscribeUser($email,$first_name,$mailChimpApiKey,$mailChimpSubDomainInit,$list_id) {
 
-
-     
     $auth = base64_encode( 'user:'.$mailChimpApiKey);
-        
+
     $data = array(
         'apikey'        => $mailChimpApiKey,
         'email_address' => $email,
         'status'        => 'subscribed',
         'merge_fields'  => array(
             'FNAME' => $first_name
-            )    
+            )
         );
 
     $json_data = json_encode($data);
-     
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://'.$mailChimpSubDomainInit.'api.mailchimp.com/3.0/lists/'.$list_id.'/members/'.md5(strtolower($email)));
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
@@ -28,30 +27,30 @@ class MailChimpUserTest {
     curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_POST, true);    
+    curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");  
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
-     
+
     $result = curl_exec($ch);
     return $result;
   }
 
   public function unSubscribeUser($email,$first_name,$mailChimpApiKey,$mailChimpSubDomainInit,$list_id) {
-     
+
     $auth = base64_encode( 'user:'.$mailChimpApiKey);
-        
+
     $data = array(
         'apikey'        => $mailChimpApiKey,
         'email_address' => $email,
         'status'        => 'unsubscribed',
         'merge_fields'  => array(
             'FNAME' => $first_name
-            )    
+            )
         );
 
     $json_data = json_encode($data);
-     
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://'.$mailChimpSubDomainInit.'api.mailchimp.com/3.0/lists/'.$list_id.'/members/'.md5(strtolower($email)));
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
@@ -59,13 +58,20 @@ class MailChimpUserTest {
     curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_POST, true);    
+    curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");  
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
-     
+
     $result = curl_exec($ch);
     return $result;
   }
 }
+
+$mailChimpService = new MailChimpService();
+$mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
+$mailChimpApiKey = $mailChimpService->mailChimpApiKey = getenv("mailChimpApiKey");
+$list_id = $mailChimpService->list_id = getenv('mailChimpListId');
+
+echo $mailChimpSubDomainInit."-".$mailChimpApiKey."-".$list_id;
 ?>
