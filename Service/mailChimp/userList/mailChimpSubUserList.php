1ea429d11184fe2bd88e7c71868b452f8aa717a8
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-03 20:16:30 +0530

    TIMEOUT REMOVED

--- Service/mailChimp/userList/mailChimpSubUserList.php
+++ Service/mailChimp/userList/mailChimpSubUserList.php
@@ -14,7 +14,7 @@ class mailChimpSubUserList {
         'Authorization: Basic '.$auth));
     curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    // curl_setopt($ch, CURLOPT_TIMEOUT, 10);
     // curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
