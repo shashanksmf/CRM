39dec4cfa57a5d16b4a8f74691e52df805346a2e
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-07 22:35:08 +0530

    addMailChimpList.php to add list

---
+++ Service/addMailChimpList.php
@@ -0,0 +1,70 @@
+<?php
+ob_start();
+// $headers = apache_request_headers();
+// $headers = $headers['token'];
+// require_once("./token/validateToken.php");
+
+// $userId = @$_GET['userId'];
+// $apiKey = @$_GET['apiKey'];
+// $listId = @$_GET['listId'];
+// $listName = @$_GET['listName'];
+
+require_once("./mailChimp/list/checkListId.php");
+require_once(".../../../Controller/mailChimpConfig.php");
+require_once(".../../../Controller/mailChimpService.php");
+
+$mailChimpService = new MailChimpService();
+$mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
+$mailChimpApiKey = $mailChimpService->mailChimpApiKey = getenv("mailChimpApiKey");
+$list_id = $mailChimpService->list_id = getenv('mailChimpListId');
+
+$checkListId = new checkListId();
+$result = $checkListId->key($mailChimpApiKey,$mailChimpSubDomainInit,$list_id);
+echo "checkListId",$result;
+
+// $result = json_decode($result, true);
+// $responseArr = array();
+// $responseArr = $result;
+
+// if ($responseArr['account_id'] != "") {
+//     $responseArr['result'] = true;
+//     // echo $responseArr;
+// } 
+// else {
+//     $responseArr['result'] = false;
+//     exit $responseArr;
+// }
+
+// require_once("../Controller/StaticDBCon.php");
+// $conn = new mysqli(StaticDBCon::$servername, StaticDBCon::$username, StaticDBCon::$password, StaticDBCon::$dbname);
+// // Create connection
+
+// $responseArr = array();
+// if (!$conn) {
+// //echo "not conn";
+//     $responseArr["result"] = false;
+//     $responseArr["details"] =  mysqli_connect_error();
+//     die($responseArr);
+// }
+// mysqli_set_charset($conn,"utf8");
+// $sql = "INSERT INTO .mailchimpdetails (userId,apiKey,listId,listName)
+// VALUES ('".$userId."','".$apiKey."','".$listId."','".$listName."')";
+
+// if (mysqli_query($conn, $sql)) {
+// //echo "if";
+//     $responseArr["result"] = true;
+//     $last_id = mysqli_insert_id($conn);
+//     $responseArr["lastId"] = $last_id;
+
+//     echo json_encode($responseArr);
+// } else {
+// //echo "else".mysqli_error($conn);
+//     $responseArr["result"] = false;
+//     $responseArr["details"] = mysqli_error($conn);
+//     echo json_encode($responseArr);
+//    // echo "Error updating record: " . mysqli_error($conn);
+// }
+
+// mysqli_close($conn);
+
+?>
