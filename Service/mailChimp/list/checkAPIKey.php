c2da268b9033cec6e8ff9d792716b5eab6ed07a5
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-07 17:26:23 +0530

    added list folder in mailChimp folder

---
+++ Service/mailChimp/list/checkAPIKey.php
@@ -0,0 +1,28 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+// class checkAPIKey {
+
+//   public function key($mailChimpApiKey,$mailChimpSubDomainInit) {
+    $mailChimpApiKey = "29d5edbfcd350d88255fbd6c3b961a8e-us14";
+   
+    $auth = base64_encode( 'user:'.$mailChimpApiKey);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://usX.api.mailchimp.com/3.0/');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+        'Authorization: Basic '.$auth));
+    curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    // curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
+    $result = curl_exec($ch);
+    echo "result" .$result;
+//   }
+// }
+
+?>
---
+++ Service/mailChimp/list/createList.php
@@ -0,0 +1,27 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+class createList {
+
+  public function list($mailChimpApiKey,$mailChimpSubDomainInit) {
+
+    $auth = base64_encode( 'user:'.$mailChimpApiKey);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://'.$mailChimpSubDomainInit.'api.mailchimp.com/3.0/lists');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+        'Authorization: Basic '.$auth));
+    curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    // curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+    $result = curl_exec($ch);
+    return $result;
+  }
+}
+
+?>
