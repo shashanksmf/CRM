6c52af0ba8f884b129a98ce04abaeedf0c867e28
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-08 14:53:25 +0530

    test

--- Service/addMailChimpId.php
+++ Service/addMailChimpId.php
@@ -1,69 +1,73 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-require_once("./phpHeader/getHeader.php");
+// error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// require_once("./phpHeader/getHeader.php");
 
-header("Access-Control-Allow-Origin: *");
-$headers = apache_request_headers();
-$headers = $headers['token'];
-require_once("./token/validateToken.php");
+// header("Access-Control-Allow-Origin: *");
+// $headers = apache_request_headers();
+// $headers = $headers['token'];
+// require_once("./token/validateToken.php");
 
-$userId = @$_GET['userId'];
-$apiKey = @$_GET['apiKey'];
+// $userId = @$_GET['userId'];
+// $apiKey = @$_GET['apiKey'];
 
 require_once("./mailChimp/list/checkAPIKey.php");
 require_once(".../../../Controller/mailChimpConfig.php");
 require_once(".../../../Controller/mailChimpService.php");
 
-$mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
+            $mailChimpService = new MailChimpService();
+            $mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
+            $mailChimpApiKey = $mailChimpService->mailChimpApiKey = getenv("mailChimpApiKey");
+            $list_id = $mailChimpService->list_id = getenv('mailChimpListId');
+
 
 $checkAPIKey = new checkAPIKey();
 $result = $checkAPIKey->key($mailChimpApiKey,$mailChimpSubDomainInit);
-// echo $result;
+echo $result;
 
-$result = json_decode($result, true);
-$responseArr = array();
-$responseArr = $result;
+// $result = json_decode($result, true);
+// $responseArr = array();
+// $responseArr = $result;
 
-if ($responseArr['account_id'] != "") {
-    $responseArr['result'] = true;
-    // echo $responseArr;
-} 
-else {
-    $responseArr['result'] = false;
-    exit $responseArr['result'];
-}
+// if ($responseArr['account_id'] != "") {
+//     $responseArr['result'] = true;
+//     // echo $responseArr;
+// } 
+// else {
+//     $responseArr['result'] = false;
+//     exit $responseArr['result'];
+// }
 
-require_once("../Controller/StaticDBCon.php");
-$conn = new mysqli(StaticDBCon::$servername, StaticDBCon::$username, StaticDBCon::$password, StaticDBCon::$dbname);
-// Create connection
+// require_once("../Controller/StaticDBCon.php");
+// $conn = new mysqli(StaticDBCon::$servername, StaticDBCon::$username, StaticDBCon::$password, StaticDBCon::$dbname);
+// // Create connection
 
-$responseArr = array();
-if (!$conn) {
-//echo "not conn";
-    $responseArr["result"] = false;
-    $responseArr["details"] =  mysqli_connect_error();
-    die($responseArr);
-}
-mysqli_set_charset($conn,"utf8");
-$sql = "INSERT INTO .mailchimpdetails (userId,apiKey,listId,listName)
-VALUES ('".$userId."','".$apiKey."','".$listId."','".$listName."')";
+// $responseArr = array();
+// if (!$conn) {
+// //echo "not conn";
+//     $responseArr["result"] = false;
+//     $responseArr["details"] =  mysqli_connect_error();
+//     die($responseArr);
+// }
+// mysqli_set_charset($conn,"utf8");
+// $sql = "INSERT INTO .mailchimpdetails (userId,apiKey,listId,listName)
+// VALUES ('".$userId."','".$apiKey."','".$listId."','".$listName."')";
 
-if (mysqli_query($conn, $sql)) {
-//echo "if";
-    $responseArr["result"] = true;
-    $last_id = mysqli_insert_id($conn);
-    $responseArr["lastId"] = $last_id;
+// if (mysqli_query($conn, $sql)) {
+// //echo "if";
+//     $responseArr["result"] = true;
+//     $last_id = mysqli_insert_id($conn);
+//     $responseArr["lastId"] = $last_id;
 
-    echo json_encode($responseArr);
-} else {
-//echo "else".mysqli_error($conn);
-    $responseArr["result"] = false;
-    $responseArr["details"] = mysqli_error($conn);
-    echo json_encode($responseArr);
-   // echo "Error updating record: " . mysqli_error($conn);
-}
+//     echo json_encode($responseArr);
+// } else {
+// //echo "else".mysqli_error($conn);
+//     $responseArr["result"] = false;
+//     $responseArr["details"] = mysqli_error($conn);
+//     echo json_encode($responseArr);
+//    // echo "Error updating record: " . mysqli_error($conn);
+// }
 
-mysqli_close($conn);
+// mysqli_close($conn);
 
 ?>
