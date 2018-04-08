e565aef5b9f91ee561c432be120011a0699f2375
shashank <shashanksmf@outlook.com>
2018-04-08 11:35:04 +0530

    undeined index token error test

--- Service/GetEmplData.php
+++ Service/GetEmplData.php
@@ -5,12 +5,11 @@ ini_set('display_errors', '1');
 require_once("./phpHeader/getHeader.php");
 
 ob_start();
-
 	//Link -> localhost/wehnc/Service/GetEmplData.php
 header("Access-Control-Allow-Origin: *");
 $headers = apache_request_headers();
 print_r($headers);
-$headers = $headers['token'];
+$headers = $headers['TOKEN'];
 require_once("./token/validateToken.php");
 
 
