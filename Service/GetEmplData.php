e7f8315bb50af8db5d90b7e7b4cfcdefdb6b600f
shashank <shashanksmf@outlook.com>
2018-04-09 23:47:47 +0530

    header('Access-Control-Allow-Headers: token'); 	in GetEmplData.php

--- Service/GetEmplData.php
+++ Service/GetEmplData.php
@@ -1,5 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: token');
+
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 require_once("./phpHeader/getHeader.php");
