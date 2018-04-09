d8a3e6c117a4be50d73781aeb30ccf1fdd52ca55
shashank <shashanksmf@outlook.com>
2018-04-09 23:09:20 +0530

    Test

--- js/config.js
+++ js/config.js
@@ -1743,16 +1743,16 @@ angular
   .config(config)
   .run(["$rootScope", "$state", "API", "$location", function($rootScope, $state,
     API, $location) {
-    $rootScope.$on('$stateChangeStart', function(event, toState, fromState) {
-
-
-      if (!localStorage.getItem("userEmail") && toState.name != 'login') {
-        if (toState.name == 'register') {
-          return;
-        }
-        $state.go('login');
-        event.preventDefault();
-      }
-      return;
-    });
+    // $rootScope.$on('$stateChangeStart', function(event, toState, fromState) {
+    //
+    //
+    //   if (!localStorage.getItem("userEmail") && toState.name != 'login') {
+    //     if (toState.name == 'register') {
+    //       return;
+    //     }
+    //     $state.go('login');
+    //     event.preventDefault();
+    //   }
+    //   return;
+    // });
   }]);
