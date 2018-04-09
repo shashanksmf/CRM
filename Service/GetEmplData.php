ff74f10bcaa6b6fd2833962240d93cc713a33ea6
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-09 10:49:26 +0530

    test

--- Service/GetEmplData.php
+++ Service/GetEmplData.php
@@ -1,15 +1,16 @@
 <?php
 
-// error_reporting(E_ALL);
-// ini_set('display_errors', '1');
-// require_once("./phpHeader/getHeader.php");
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once("./phpHeader/getHeader.php");
 
 header("Access-Control-Allow-Origin: *");
 $headers = apache_request_headers();
-$headers = $headers['token'];
 print_r($headers);
-echo "$headers",$headers;
+$headers = $headers['token'];
+echo $headers['token'];
 require_once("./token/validateToken.php");
+// print_r($headers);
 //Link -> localhost/wehnc/Service/GetEmplData.php
 
 
