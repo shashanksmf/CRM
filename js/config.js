3ac8dc56a1bfa9cdcbcb0f342eb784eedbfa1f78
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-09 12:47:18 +0530

    test

--- js/config.js
+++ js/config.js
@@ -10,6 +10,9 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider,
   IdleProvider, KeepaliveProvider,$httpProvider) {
   $httpProvider.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
   $httpProvider.defaults.headers.common['Access-Control-Request-Method'] = 'GET';
+  $httpProvider.defaults.headers.common = {
+          token:response.data.token
+        }
   // Configure Idle settings
   IdleProvider.idle(5); // in seconds
   IdleProvider.timeout(120); // in seconds
