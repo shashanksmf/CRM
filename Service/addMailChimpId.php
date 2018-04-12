eb0349ab90b7e0d53120d8b8fe6756ac5b51c25b
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-12 17:12:10 +0530

    test

--- Service/addMailChimpId.php
+++ Service/addMailChimpId.php
@@ -11,35 +11,36 @@ require_once("./phpHeader/getHeader.php");
 $headers = apache_request_headers();
 require_once("./token/validateToken.php");
 
+exit();
 
-$userId = @$_GET['userId'];
-$apiKey = @$_GET['apiKey'];
+// $userId = @$_GET['userId'];
+// $apiKey = @$_GET['apiKey'];
 
-require_once("./mailChimp/list/checkAPIKey.php");
-require_once(".../../../Controller/mailChimpConfig.php");
-require_once(".../../../Controller/mailChimpService.php");
+// require_once("./mailChimp/list/checkAPIKey.php");
+// require_once(".../../../Controller/mailChimpConfig.php");
+// require_once(".../../../Controller/mailChimpService.php");
 
-$mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
+// $mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
 
-$checkAPIKey = new checkAPIKey();
-$result = $checkAPIKey->key($apiKey,$mailChimpSubDomainInit);
-echo "echoResult".$result;
+// $checkAPIKey = new checkAPIKey();
+// $result = $checkAPIKey->key($apiKey,$mailChimpSubDomainInit);
+// echo "echoResult".$result;
 
-$result = json_decode($result, true);
-echo "res".$result;
-$resultArr = array();
-$resultArr = $result;
-echo $resultArr;
+// $result = json_decode($result, true);
+// echo "res".$result;
+// $resultArr = array();
+// $resultArr = $result;
+// echo $resultArr;
 
-if ($resultArr['account_id']) {
-    // $resultArr['result'] = true;
-    // exit $resultArr;
-} 
-else {
-    $resultArr['result'] = false;
-    $resultArr['errorType'] = "apiKey";
-    exit $resultArr;
-}
+// if ($resultArr['account_id']) {
+//     // $resultArr['result'] = true;
+//     // exit $resultArr;
+// } 
+// else {
+//     $resultArr['result'] = false;
+//     $resultArr['errorType'] = "apiKey";
+//     exit $resultArr;
+// }
 
 // require_once("../Controller/StaticDBCon.php");
 // $conn = new mysqli(StaticDBCon::$servername, StaticDBCon::$username, StaticDBCon::$password, StaticDBCon::$dbname);
