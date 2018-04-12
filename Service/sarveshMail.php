e4667ffdad3fc5969bb7e75d267f057b80905a80
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-12 19:00:21 +0530

    mail testing

--- Service/sarveshMail.php
+++ Service/sarveshMail.php
@@ -1,9 +1,21 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
 $to = "judy@techinvento.com";
 $subject = "My subject";
 $txt = "Hello world!";
-$headers = "From: shashank@techinvento.com" . "\r\n" .
-"";
+$headers .= "From: shashank@techinvento.com" . "\r\n";
 
-mail($to,$subject,$txt,$headers);
+if(mail($to,$subject,$txt,$headers)) {
+	echo "send successfully";
+}
+else {
+	echo "failed";
+}
 ?>
