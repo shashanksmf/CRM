8be699d3de6ab6a9b6ce1c724a27569c2b3e95b3
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-12 15:24:50 +0530

    test

--- Service/mailChimp/list/checkAPIKey.php
+++ Service/mailChimp/list/checkAPIKey.php
@@ -1,9 +1,9 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
     header("Access-Control-Allow-Origin: *");
     header('Access-Control-Allow-Headers: Origin, token, Host');
 
-    error_reporting(E_ALL);
-    ini_set('display_errors', '1');
     require_once("../.././phpHeader/getHeader.php");
 
 class checkAPIKey {
