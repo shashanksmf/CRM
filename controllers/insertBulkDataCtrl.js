089b47acc9483a6fc1481e5d9b7cfa82c480f4d5
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-20 17:10:44 +0530

    added validation for insertBulkData

--- controllers/insertBulkDataCtrl.js
+++ controllers/insertBulkDataCtrl.js
@@ -1,66 +1,78 @@
 var inspinia = angular.module('inspinia');
 
-inspinia.controller("insertBulkDataCtrl", ['$scope','$rootScope','$http','$q','API','$state','$stateParams','$timeout','crmconfig', function ($scope,$rootScope,$http,$q,API,$state,$stateParams,$timeout,crmconfig) {
-	
+inspinia.controller("insertBulkDataCtrl", ['$scope', '$rootScope', '$http', '$q', 'API', '$state', '$stateParams', '$timeout', 'crmconfig', function($scope, $rootScope, $http, $q, API, $state, $stateParams, $timeout, crmconfig) {
+
 	var userId = $rootScope.userId || localStorage.getItem("userId");
 	$scope.IsVisible = false;
 	$scope.LinkVisible = false;
 
-	$scope.read = function (workbook) {
+	$scope.read = function(workbook) {
 		$scope.LinkVisible = false;
 
-		var headerNames = XLSX.utils.sheet_to_json( workbook.Sheets[workbook.SheetNames[0]], { header: 1 })[0];
-		var data = XLSX.utils.sheet_to_json( workbook.Sheets[workbook.SheetNames[0]]);
+		var headerNames = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {
+			header: 1
+		})[0];
+		var data = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
 
 		console.log(headerNames);
-		// console.log(data);
-		if(headerNames.indexOf("Firstname" && "Surname" && "E-mail address" && "Company" 
-			&& "Job Role" && "Office Address" && "Company Website" 
-			&& "Industry" && "Country") !== -1){
-			console.log("true");
-			$scope.IsVisible = true;
-			API.insertBulkData(userId,data).then(function(response){
-				console.log("insertBulkData",response);
-				$tId = response.data.tId;
-				if(response.data.result) {
-		            alert("Records inserted");
-		            $scope.IsVisible = false;
-		            $scope.LinkVisible = true;
-		        }
-		        else if(response.data.errorType && response.data.errorType == "token"){
-	                $('#tokenErrorModalLabel').html(response.data.details);
-	                $('#tokenErrorModal').modal("show");
-	                $('#tokenErrorModalBtn').click(function(){
-	                    $('#tokenErrorModal').modal("hide");
-	            	})
-	         	}
-				
-			});
+		var tempArr=["Firstname", "Surname", "E-mail address", "Company",
+				"Job Role", "Office Address", "Company Website",
+				"Industry", "Country"];
+
+		for (var i = headerNames.length - 1; i >= 0; i--) {
+			for (var j = 0; j < tempArr.length; j++) {
+				if(headerNames[i]==tempArr[j]){
+					tempArr.splice(j,1);
+					break;
+				}
+			};
+		};
+		if(tempArr.length>0){
+			console.log(tempArr.join(', '));
+			$err = tempArr.join(', ') + " this column are missing in your excel file";
+			alert($err);
 		}
 		else {
-			$scope.LinkVisible = false;
-			console.log("false");
-			alert("Please add data in file in following column format: Firstname, Surname, E-mail address, Company, Job Role, Office Address, Company Website, Industry, Country.");
+			$scope.IsVisible = true;
+			API.insertBulkData(userId, data).then(function(response) {
+				console.log("insertBulkData", response);
+				$tId = response.data.tId;
+				if (response.data.result) {
+					alert("Records inserted successfully!");
+					$scope.IsVisible = false;
+					$scope.LinkVisible = true;
+				} else if (response.data.errorType && response.data.errorType == "token") {
+					$('#tokenErrorModalLabel').html(response.data.details);
+					$('#tokenErrorModal').modal("show");
+					$('#tokenErrorModalBtn').click(function() {
+						$('#tokenErrorModal').modal("hide");
+					})
+				}
+
+			});
 		}
-		
+
+		// console.log(data);
 		// for (var row in data)
 		// 	{
-				// console.log(data[row]);
-				// Object.keys(data[row]).forEach(function(key) {
-					// console.log("Key = >" + key);
-					// console.log("Value => " + data[row][key]);
-					// console.log("===========================");
-				// });
+		// console.log(data[row]);
+		// Object.keys(data[row]).forEach(function(key) {
+		// console.log("Key = >" + key);
+		// console.log("Value => " + data[row][key]);
+		// console.log("===========================");
+		// });
 		// }
 	}
 
-	$scope.error = function (e) {
+	$scope.error = function(e) {
 		console.log(e);
 		alert("Unsupported file please choose excel file");
 	}
 
 	$scope.trDetails = function() {
-		$state.go('dashboards.transactionDetails', {tId: $tId});
+		$state.go('dashboards.transactionDetails', {
+			tId: $tId
+		});
 	}
 
 }])
