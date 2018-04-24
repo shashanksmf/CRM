c85dfa5ba39f64beec7bc8d485e94efbe1041e6d
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-24 17:59:12 +0530

    test

--- Service/TwilioSMS/sendSMSTwilio.php
+++ Service/TwilioSMS/sendSMSTwilio.php
@@ -15,22 +15,16 @@ $account_sid = 'ACd6174cef3f620aee42f1c89fc9d6a7fe';
 $auth_token = '213c7b2c415da489eac89130e4d180b8';
 $client = new Client($account_sid, $auth_token);
 
-$messages = $client->accounts("ACd6174cef3f620aee42f1c89fc9d6a7fe") 
-  ->messages->create("+919511762987", array( 
-        'From' => "+15128588607",   
-        'body' => 'Hey Jenny! Good luck on the bar exam!'     
-  ));
-
-// // Use the client to do fun stuff like send text messages!
-// $client->messages->create(
-//     // the number you'd like to send the message to
-//     '+919511762987',
-//     array(
-//         // A Twilio phone number you purchased at twilio.com/console
-//         'from' => '+15128588607',
-//         // the body of the text message you'd like to send
-//         'body' => 'Hey Jenny! Good luck on the bar exam!'
-//     )
-// );
+// Use the client to do fun stuff like send text messages!
+$client->messages->create(
+    // the number you'd like to send the message to
+    '+919511762987',
+    array(
+        // A Twilio phone number you purchased at twilio.com/console
+        'from' => '+15128588607',
+        // the body of the text message you'd like to send
+        'body' => 'Hey Jenny! Good luck on the bar exam!'
+    )
+);
 
 ?>
