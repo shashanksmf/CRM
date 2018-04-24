c65a70438b8e9f294f09328368666bc94a8282f1
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-24 12:09:18 +0530

    test

--- Service/token/validateToken.php
+++ Service/token/validateToken.php
@@ -12,51 +12,51 @@ use ReallySimpleJWT\TokenBuilder;
 
 class validateToken {
 
-      public function validate($token) {
-    	$validator = new TokenValidator;
-			$secret =  'SecretSuperstar@99';
+	public function validate($token) {
+		$validator = new TokenValidator;
+		$secret =  'SecretSuperstar@99';
 			// $token = $_POST['token'];
-			$responseArr = array();
-			$tokenExp = '';
+		$responseArr = array();
+		$tokenExp = '';
 
 			// print_r($validator->splitToken($token));
 			// echo Carbon::now()->addMinutes(0)->toDateTimeString();
 			// print_r($getPayload = Token::getPayload($token));
 
-			$getPayload = Token::getPayload($token);
+		$getPayload = Token::getPayload($token);
 
-			try {
-			  $result = Token::validate($token, $secret);
-			  $tokenExp = $validator->splitToken($token)->validateExpiration()->validateSignature($secret);
-			}
-			catch(Exception $e) {
-			   $responseArr['result'] = false;
-			   $responseArr['errorType'] = 'token';
-			   $responseArr["details"] = $e->getMessage();
-			   $responseArr['getPayload'] = json_decode($getPayload);
+		try {
+			$result = Token::validate($token, $secret);
+			$tokenExp = $validator->splitToken($token)->validateExpiration()->validateSignature($secret);
+		}
+		catch(Exception $e) {
+			$responseArr['result'] = false;
+			$responseArr['errorType'] = 'token';
+			$responseArr["details"] = $e->getMessage();
+			$responseArr['getPayload'] = json_decode($getPayload);
 			   // exit(json_encode($responseArr));
-			   return $responseArr;
-			}
+			return $responseArr;
+		}
 
 			// $responseArr['result'] = $result;
 			// $responseArr['exp'] = $tokenExp;
 			// $responseArr['getPayload'] = json_decode($getPayload);
 			// echo json_encode($responseArr);
-		}
+	}
 }
 
+// if (array_key_exists('TOKEN', $headers)){
+if(isset($headers['TOKEN']) && !empty($headers['TOKEN'])){
 	$validateToken = new validateToken();
-  // if (array_key_exists('TOKEN', $headers)){
-  if(isset($headers['TOKEN']) && !empty($headers['TOKEN'])){
-    $result = $validateToken->validate($headers['TOKEN']);
+	$result = $validateToken->validate($headers['TOKEN']);
     // echo json_encode($result);
-    if (strlen($result['details']) > 0 && $result['result'] == false) {
-      exit(json_encode($result));
-    }
-  }
-  else {
-    $result['errorType'] = 'tokenExp';
-    exit(json_encode($result));
-  }
+	if (strlen($result['details']) > 0 && $result['result'] == false) {
+		exit(json_encode($result));
+	}
+}
+else {
+	$result['errorType'] = 'token';
+	exit(json_encode($result));
+}
 
 ?>
