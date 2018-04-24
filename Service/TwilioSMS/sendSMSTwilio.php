af0ce03d17e507e23e8c4694d96dc3dfe363a593
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-24 17:57:38 +0530

    test

--- Service/TwilioSMS/sendSMSTwilio.php
+++ Service/TwilioSMS/sendSMSTwilio.php
@@ -11,20 +11,26 @@ include_once('../../vendor/autoload.php');
 use Twilio\Rest\Client;
 
 // Your Account SID and Auth Token from twilio.com/console
-$account_sid = 'ACd8c4a4ec09d8a4198292669ba559f661';
-$auth_token = 'fbdfb06b92862c22145ba03b68ce5298';
+$account_sid = 'ACd6174cef3f620aee42f1c89fc9d6a7fe';
+$auth_token = '213c7b2c415da489eac89130e4d180b8';
 $client = new Client($account_sid, $auth_token);
 
-// Use the client to do fun stuff like send text messages!
-$client->messages->create(
-    // the number you'd like to send the message to
-    '+919511762987',
-    array(
-        // A Twilio phone number you purchased at twilio.com/console
-        'from' => '+15128588607',
-        // the body of the text message you'd like to send
-        'body' => 'Hey Jenny! Good luck on the bar exam!'
-    )
-);
+$messages = $client->accounts("ACd6174cef3f620aee42f1c89fc9d6a7fe") 
+  ->messages->create("+919511762987", array( 
+        'From' => "+15128588607",   
+        'body' => 'Hey Jenny! Good luck on the bar exam!'     
+  ));
+
+// // Use the client to do fun stuff like send text messages!
+// $client->messages->create(
+//     // the number you'd like to send the message to
+//     '+919511762987',
+//     array(
+//         // A Twilio phone number you purchased at twilio.com/console
+//         'from' => '+15128588607',
+//         // the body of the text message you'd like to send
+//         'body' => 'Hey Jenny! Good luck on the bar exam!'
+//     )
+// );
 
 ?>
