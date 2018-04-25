6bb6938b742f2af2cc685854e4fa96997cbd58da
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-04-25 12:08:31 +0530

    test

--- Service/TwilioSMS/sendSMSTwilio.php
+++ Service/TwilioSMS/sendSMSTwilio.php
@@ -15,16 +15,20 @@ $account_sid = 'ACd6174cef3f620aee42f1c89fc9d6a7fe';
 $auth_token = '213c7b2c415da489eac89130e4d180b8';
 $client = new Client($account_sid, $auth_token);
 
-// Use the client to do fun stuff like send text messages!
-$client->messages->create(
-    // the number you'd like to send the message to
-    '+919511762987', '+919822097124',
-    array(
-        // A Twilio phone number you purchased at twilio.com/console
-        'from' => '+15128588607',
-        // the body of the text message you'd like to send
-        'body' => 'Hey! Good luck!'
-    )
-);
+$noArr = array('+919511762987', '+919822097124');
+$arrlength = count($noArr);
+for ($i=0; $i < $arrlength; $i++) { 
+	// Use the client to do fun stuff like send text messages!
+	$client->messages->create(
+	    // the number you'd like to send the message to
+	    $noArr[$i],
+	    array(
+	        // A Twilio phone number you purchased at twilio.com/console
+	        'from' => '+15128588607',
+	        // the body of the text message you'd like to send
+	        'body' => 'Hey! Good luck!'
+	    )
+	);
+}
 
 ?>
