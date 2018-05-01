4eb06a0e2a42ac4babf78a3038a112fd5c24fb6d
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-01 17:01:09 +0530

    test

--- Service/googleLogIn.php
+++ Service/googleLogIn.php
@@ -17,12 +17,12 @@ $client->setRedirectUri('https://' . $_SERVER['HTTP_HOST']);
 $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 if (! isset($_GET['code'])) {
   $auth_url = $client->createAuthUrl();
-  // echo $auth_url;
+  echo $auth_url;
   header('Location:' . filter_var($auth_url, FILTER_SANITIZE_URL));
 } else {
   $client->authenticate($_GET['code']);
   $_SESSION['access_token'] = $client->getAccessToken();
-  // echo $_SESSION['access_token'];
+  echo $_SESSION['access_token'];
   $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'];
   header('Location:' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
