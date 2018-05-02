47ef72136f24a91d19280d1987f8b20bd793f3fc
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-02 12:57:36 +0530

    test

--- Service/googleLogIn.php
+++ Service/googleLogIn.php
@@ -1,8 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Origin, token, Host');
-// header('Access-Control-Allow-Methods: GET');
-// header("Access-Control-Request-Method: *");
+header('Access-Control-Allow-Methods: POST, GET');
+header('Access-Control-Allow-Headers: Authorization');
+header('Access-Control-Max-Age: 1');  //1728000
+header("Content-Length: 0");
+header("Content-Type: text/plain charset=UTF-8");
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
@@ -15,8 +18,8 @@ $client = new Google_Client();
 $client->setAuthConfigFile('../credentials.json');
 $client->addScope(Google_Service_Plus::PLUS_ME);
 $httpClient = $client->authorize();
-$client->setRedirectUri('https://upsailgroup.herokuapp.com');
-// $client->setRedirectUri('https://' . $_SERVER['HTTP_HOST']);
+// $client->setRedirectUri('https://upsailgroup.herokuapp.com');
+$client->setRedirectUri('https://' . $_SERVER['HTTP_HOST']);
 $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 if (! isset($_GET['code'])) {
   echo $_GET['code'];
@@ -27,8 +30,8 @@ if (! isset($_GET['code'])) {
   $client->authenticate($_GET['code']);
   $_SESSION['access_token'] = $client->getAccessToken();
   echo $_SESSION['access_token'];
-  $redirect_uri = 'https://upsailgroup.herokuapp.com';
-  // $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'];
+  // $redirect_uri = 'https://upsailgroup.herokuapp.com';
+  $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'];
   header('Location:' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 $response = $httpClient->get('https://www.googleapis.com/plus/v1/people/me');
