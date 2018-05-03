09500c9b64a2b346a475063989c1b2d6d7bb0ffb
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-03 19:29:25 +0530

    test

--- Service/googleLogIn.php
+++ Service/googleLogIn.php
@@ -12,22 +12,6 @@ include_once('../vendor/autoload.php');
 session_start();
 
 $responseArr = array();
-// $client = new Google_Client();
-// $client->setAuthConfig('../credentials.json');
-// $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-
-// if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-//   $client->setAccessToken($_SESSION['access_token']);
-//   $drive = new Google_Service_Drive($client);
-//   $files = $drive->files->listFiles(array())->getItems();
-//   echo json_encode($files);
-// } else {
-//   $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'];
-//   // header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-//   $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
-//   echo json_encode($responseArr);
-// }
-
 $client = new Google_Client();
 $client->setAuthConfigFile('../credentials.json');
 $client->addScope(Google_Service_Plus::PLUS_ME);
