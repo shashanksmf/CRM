746760c4a80eea9b27f4e1ac01bdc154257f0e2e
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-03 16:14:17 +0530

    test

--- Service/googleLogIn.php
+++ Service/googleLogIn.php
@@ -15,47 +15,35 @@ $client = new Google_Client();
 $client->setAuthConfigFile('../credentials.json');
 $client->addScope(Google_Service_Plus::PLUS_ME);
 $httpClient = $client->authorize();
-// $access_token = $client->getAccessToken();
-// echo "access_token".$access_token."end";
-// $response = $httpClient->get('https://www.googleapis.com/plus/v1/people/me');
-// print_r($response);
-// exit;
 // $client->setRedirectUri('https://upsailgroup.herokuapp.com');
 $client->setRedirectUri('https://' . $_SERVER['HTTP_HOST']);
 $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 if (! isset($_GET['code'])) {
   $auth_url = $client->createAuthUrl();
   $_SESSION['access_token'] = $client->getAccessToken();
-  // echo "access_token".$_SESSION['access_token'];
-  // echo "auth_url".$auth_url;
   // header('Location:' . filter_var($auth_url, FILTER_SANITIZE_URL));
   // echo '<script type="text/javascript">top.location.href = "'.filter_var($auth_url, FILTER_SANITIZE_URL).'";</script>';
-  // echo '<meta http-equiv="refresh" content="0; url="'.filter_var($auth_url, FILTER_SANITIZE_URL).'">';
   $responseArr['result'] = True;
   $responseArr['access_token'] = $_SESSION['access_token'];
   $responseArr["url"] = filter_var($auth_url, FILTER_SANITIZE_URL);
-  // $response = $httpClient->get('https://www.googleapis.com/plus/v1/people/me');
-  // // print_r($response);
-  // $responseArr['email'] = json_encode($response);
-  echo json_encode($responseArr);
+  // echo json_encode($responseArr);
 } else {
   $client->authenticate($_GET['code']);
   $_SESSION['access_token'] = $client->getAccessToken();
   echo "access_token".$_SESSION['access_token'];
-  // $redirect_uri = 'https://upsailgroup.herokuapp.com';
   $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'];
-  // echo "$redirect_uri".$redirect_uri;
   // header('Location:' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
   // echo '<script type="text/javascript">top.location.href = "'.filter_var($redirect_uri, FILTER_SANITIZE_URL).'";</script>';
-  // echo '<meta http-equiv="refresh" content="0; url="'.filter_var($redirect_uri, FILTER_SANITIZE_URL).'">';
   $responseArr['result'] = False;
   $responseArr['access_token'] = $_SESSION['access_token'];
   $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
-  // $response = $httpClient->get('https://www.googleapis.com/plus/v1/people/me');
-  // // print_r($response);
-  // $responseArr['email'] = json_encode($response);
-  echo json_encode($responseArr);
+  // echo json_encode($responseArr);
 }
+$response = $httpClient->get('https://www.googleapis.com/plus/v1/people/me');
+// print_r($response);
+$responseArr['email'] = json_encode($response);
+echo json_encode($responseArr);
+
 
 // require_once("../Controller/Class_User_Login_Controller.php");
 // $controller = new UserLoginController();
