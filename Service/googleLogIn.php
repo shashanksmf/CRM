61a74188fadfea9e2605a51a1783b7677f1cfa33
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-03 18:34:56 +0530

    test

--- Service/googleLogIn.php
+++ Service/googleLogIn.php
@@ -10,76 +10,24 @@ include_once('../vendor/autoload.php');
 
 
 session_start();
-// require_once 'dbcontroller.php';
 
-//Google API PHP Library includes
-// require_once 'Google/Client.php';
-// require_once 'Google/Service/Oauth2.php';
-
-// Fill CLIENT ID, CLIENT SECRET ID, REDIRECT URI from Google Developer Console
 $responseArr = array();
- $client_id = 'oUv4b6yzrPaz_YG2nx9Toy0J';
- $client_secret = '106745707537-lqdq3l9g6l6gkim9fgqn2hqbktpslatf.apps.googleusercontent.com';
- $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'];
- $simple_api_key = 'AIzaSyCCBX6FQPBwCCqarnFKHWV9Ls1LzI2AMIU';
- 
-//Create Client Request to access Google API
 $client = new Google_Client();
-$client->setApplicationName("CRM.git");
-$client->setClientId($client_id);
-$client->setClientSecret($client_secret);
-$client->setRedirectUri($redirect_uri);
-$client->setDeveloperKey($simple_api_key);
-$client->addScope("https://www.googleapis.com/auth/userinfo.email");
+$client->setAuthConfig('../credentials.json');
+$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 
-//Send Client Request
-$objOAuthService = new Google_Service_Oauth2($client);
-
-//Logout
-if (isset($_REQUEST['logout'])) {
-  unset($_SESSION['access_token']);
-  $client->revokeToken();
-  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL)); //redirect user back to page
-  // $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
-//   echo json_encode($responseArr);
-}
-
-//Authenticate code from Google OAuth Flow
-//Add Access Token to Session
-if (isset($_GET['code'])) {
-  $client->authenticate($_GET['code']);
-  $_SESSION['access_token'] = $client->getAccessToken();
-  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-  // $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
-//   echo json_encode($responseArr);
-}
-
-//Set Access Token to make Request
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
-}
-
-//Get User Data from Google Plus
-//If New, Insert to Database
-if ($client->getAccessToken()) {
-  $userData = $objOAuthService->userinfo->get();
-  if(!empty($userData)) {
-  $objDBController = new DBController();
-  $existing_member = $objDBController->getUserByOAuthId($userData->id);
-  if(empty($existing_member)) {
-    $objDBController->insertOAuthUser($userData);
-  }
-  }
-  $_SESSION['access_token'] = $client->getAccessToken();
+  $drive = new Google_Service_Drive($client);
+  $files = $drive->files->listFiles(array())->getItems();
+  echo json_encode($files);
 } else {
-  $authUrl = $client->createAuthUrl();
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'];
+  // header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+  $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
+  echo json_encode($responseArr);
 }
-// require_once("loginpageview.php")
 
-
-// session_start();
-
-// $responseArr = array();
 // $client = new Google_Client();
 // $client->setAuthConfigFile('../credentials.json');
 // $client->addScope(Google_Service_Plus::PLUS_ME);
@@ -120,5 +68,64 @@ if ($client->getAccessToken()) {
 // $controller = new UserLoginController();
 // header('Content-Type: application/json');
 // echo $controller->checkUserEmail($email);
+
+
+// $responseArr = array();
+//  $client_id = 'oUv4b6yzrPaz_YG2nx9Toy0J';
+//  $client_secret = '106745707537-lqdq3l9g6l6gkim9fgqn2hqbktpslatf.apps.googleusercontent.com';
+//  $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'];
+//  $simple_api_key = 'AIzaSyCCBX6FQPBwCCqarnFKHWV9Ls1LzI2AMIU';
+ 
+// //Create Client Request to access Google API
+// $client = new Google_Client();
+// $client->setApplicationName("CRM.git");
+// $client->setClientId($client_id);
+// $client->setClientSecret($client_secret);
+// $client->setRedirectUri($redirect_uri);
+// $client->setDeveloperKey($simple_api_key);
+// $client->addScope("https://www.googleapis.com/auth/userinfo.email");
+
+// //Send Client Request
+// $objOAuthService = new Google_Service_Oauth2($client);
+
+// //Logout
+// if (isset($_REQUEST['logout'])) {
+//   unset($_SESSION['access_token']);
+//   $client->revokeToken();
+//   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL)); //redirect user back to page
+//   // $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
+// //   echo json_encode($responseArr);
+// }
+
+// //Authenticate code from Google OAuth Flow
+// //Add Access Token to Session
+// if (isset($_GET['code'])) {
+//   $client->authenticate($_GET['code']);
+//   $_SESSION['access_token'] = $client->getAccessToken();
+//   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+//   // $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
+// //   echo json_encode($responseArr);
+// }
+
+// //Set Access Token to make Request
+// if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+//   $client->setAccessToken($_SESSION['access_token']);
+// }
+
+// //Get User Data from Google Plus
+// //If New, Insert to Database
+// if ($client->getAccessToken()) {
+//   $userData = $objOAuthService->userinfo->get();
+//   if(!empty($userData)) {
+//   $objDBController = new DBController();
+//   $existing_member = $objDBController->getUserByOAuthId($userData->id);
+//   if(empty($existing_member)) {
+//     $objDBController->insertOAuthUser($userData);
+//   }
+//   }
+//   $_SESSION['access_token'] = $client->getAccessToken();
+// } else {
+//   $authUrl = $client->createAuthUrl();
+// }
 
 ?>
