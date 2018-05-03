697308410d7c965eb2abc26062ec08d8cb352c53
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-03 18:38:24 +0530

    test

--- Service/googleLogIn.php
+++ Service/googleLogIn.php
@@ -12,52 +12,52 @@ include_once('../vendor/autoload.php');
 session_start();
 
 $responseArr = array();
-$client = new Google_Client();
-$client->setAuthConfig('../credentials.json');
-$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-
-if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-  $client->setAccessToken($_SESSION['access_token']);
-  $drive = new Google_Service_Drive($client);
-  $files = $drive->files->listFiles(array())->getItems();
-  echo json_encode($files);
-} else {
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'];
-  // header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-  $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
-  echo json_encode($responseArr);
-}
-
 // $client = new Google_Client();
-// $client->setAuthConfigFile('../credentials.json');
-// $client->addScope(Google_Service_Plus::PLUS_ME);
-// $httpClient = $client->authorize();
-// // $client->setRedirectUri('https://upsailgroup.herokuapp.com');
-// $client->setRedirectUri('https://' . $_SERVER['HTTP_HOST']);
+// $client->setAuthConfig('../credentials.json');
 // $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-// if (! isset($_GET['code'])) {
-//   $auth_url = $client->createAuthUrl();
-//   $_SESSION['access_token'] = $client->getAccessToken();
-//   // echo '<script type="text/javascript">top.location.href = "'.filter_var($auth_url, FILTER_SANITIZE_URL).'";</script>';
-//   $responseArr['result'] = True;
-//   $responseArr['access_token'] = $_SESSION['access_token'];
-//   $responseArr["url"] = filter_var($auth_url, FILTER_SANITIZE_URL);
-//   echo json_encode($responseArr);
-//   // header('Location:' . filter_var($auth_url, FILTER_SANITIZE_URL));
-  
+
+// if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+//   $client->setAccessToken($_SESSION['access_token']);
+//   $drive = new Google_Service_Drive($client);
+//   $files = $drive->files->listFiles(array())->getItems();
+//   echo json_encode($files);
 // } else {
-//   $client->authenticate($_GET['code']);
-//   $_SESSION['access_token'] = $client->getAccessToken();
-//   echo "access_token".$_SESSION['access_token'];
-//   $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'];
-//   // echo '<script type="text/javascript">top.location.href = "'.filter_var($redirect_uri, FILTER_SANITIZE_URL).'";</script>';
-//   $responseArr['result'] = False;
-//   $responseArr['access_token'] = $_SESSION['access_token'];
+//   $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'];
+//   // header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 //   $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
 //   echo json_encode($responseArr);
-//   // header('Location:' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-  
 // }
+
+$client = new Google_Client();
+$client->setAuthConfigFile('../credentials.json');
+$client->addScope(Google_Service_Plus::PLUS_ME);
+$httpClient = $client->authorize();
+// $client->setRedirectUri('https://upsailgroup.herokuapp.com');
+$client->setRedirectUri('https://' . $_SERVER['HTTP_HOST']);
+$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+if (! isset($_GET['code'])) {
+  $auth_url = $client->createAuthUrl();
+  $_SESSION['access_token'] = $client->getAccessToken();
+  // echo '<script type="text/javascript">top.location.href = "'.filter_var($auth_url, FILTER_SANITIZE_URL).'";</script>';
+  $responseArr['result'] = True;
+  $responseArr['access_token'] = $_SESSION['access_token'];
+  $responseArr["url"] = filter_var($auth_url, FILTER_SANITIZE_URL);
+  echo json_encode($responseArr);
+  // header('Location:' . filter_var($auth_url, FILTER_SANITIZE_URL));
+  
+} else {
+  $client->authenticate($_GET['code']);
+  $_SESSION['access_token'] = $client->getAccessToken();
+  echo "access_token".$_SESSION['access_token'];
+  $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'];
+  // echo '<script type="text/javascript">top.location.href = "'.filter_var($redirect_uri, FILTER_SANITIZE_URL).'";</script>';
+  $responseArr['result'] = False;
+  $responseArr['access_token'] = $_SESSION['access_token'];
+  $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
+  echo json_encode($responseArr);
+  // header('Location:' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+  
+}
 // $response = $httpClient->get('https://www.googleapis.com/plus/v1/people/me');
 // print_r($response);
 // $responseArr['email'] = json_encode($response);
