ecdc5e282e7764f08d66f45d4c6f311490e2a4fa
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-04 11:43:37 +0530

    test

--- Service/googleLogIn.php
+++ Service/googleLogIn.php
@@ -64,7 +64,7 @@ $client->setClientId($client_id);
 $client->setClientSecret($client_secret);
 $client->setRedirectUri($redirect_uri);
 $client->setDeveloperKey($simple_api_key);
-$client->addScope(Google_Service_Plus::PLUS_ME);
+$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 
 //Send Client Request
 $objOAuthService = new Google_Service_Oauth2($client);
