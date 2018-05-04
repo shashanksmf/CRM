8fa1e8973b1413875455203842231cc6b54fe606
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-04 11:37:59 +0530

    test

--- Service/googleLogIn.php
+++ Service/googleLogIn.php
@@ -64,7 +64,7 @@ $client->setClientId($client_id);
 $client->setClientSecret($client_secret);
 $client->setRedirectUri($redirect_uri);
 $client->setDeveloperKey($simple_api_key);
-$client->addScope("https://www.googleapis.com/auth/userinfo.email");
+// $client->addScope("https://www.googleapis.com/auth/userinfo.email");
 
 //Send Client Request
 $objOAuthService = new Google_Service_Oauth2($client);
