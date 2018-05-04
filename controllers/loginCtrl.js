0da3ec2bc19b5c758d52c849f9609d556b134dfe
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-04 11:32:41 +0530

    test

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -43,6 +43,7 @@ inspinia.controller('loginCtrl', ['$scope','$rootScope','$http','$q','API','$sta
 		API.googleLogIn().then(function(response){
 			console.log(response);
 			// window.location = response.data.url;
+			// window.location = response.data.url;
 		});
 	}
 
