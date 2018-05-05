558d8dd0ec4c9b1372806e28a0da87ebf2e1cc0d
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-05 20:10:58 +0530

    test

--- Service/facebookLogIn.php
+++ Service/facebookLogIn.php
@@ -97,18 +97,18 @@ if(isset($accessToken)){
     // $user = new User();
     
     // Insert or update user data to the database
-    $fbUserData = array(
-        'oauth_provider'=> 'facebook',
-        'oauth_uid'     => $fbUserProfile['id'],
-        'first_name'    => $fbUserProfile['first_name'],
-        'last_name'     => $fbUserProfile['last_name'],
-        'email'         => $fbUserProfile['email'],
-        'gender'        => $fbUserProfile['gender'],
-        'locale'        => $fbUserProfile['locale'],
-        'cover'         => $fbUserProfile['cover']['source'],
-        'picture'       => $fbUserProfile['picture']['url'],
-        'link'          => $fbUserProfile['link']
-    );
+    // $fbUserData = array(
+    //     'oauth_provider'=> 'facebook',
+    //     'oauth_uid'     => $fbUserProfile['id'],
+    //     'first_name'    => $fbUserProfile['first_name'],
+    //     'last_name'     => $fbUserProfile['last_name'],
+    //     'email'         => $fbUserProfile['email'],
+    //     'gender'        => $fbUserProfile['gender'],
+    //     'locale'        => $fbUserProfile['locale'],
+    //     'cover'         => $fbUserProfile['cover']['source'],
+    //     'picture'       => $fbUserProfile['picture']['url'],
+    //     'link'          => $fbUserProfile['link']
+    // );
     $responseArr['fbUserData'] = $fbUserData;
     // $userData = $user->checkUser($fbUserData);
     
