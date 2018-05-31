8b046629e45136cef0acd9c57f528cc7dc46c6b7
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-05-31 13:19:32 +0530

    test

--- Service/googleLogIn.php
+++ Service/googleLogIn.php
@@ -18,7 +18,7 @@ $responseArr = array();
 $client_id = '106745707537-lqdq3l9g6l6gkim9fgqn2hqbktpslatf.apps.googleusercontent.com';
 $client_secret = 'oUv4b6yzrPaz_YG2nx9Toy0J';
 // $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'];
-$redirect_uri = 'https://upsailgroup.herokuapp.com/Service/googleLogIn.php';
+$redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] .'/Service/googleLogIn.php';
 $simple_api_key = 'AIzaSyCCBX6FQPBwCCqarnFKHWV9Ls1LzI2AMIU';
 
 //Create Client Request to access Google API
@@ -46,8 +46,8 @@ if (isset($_REQUEST['logout'])) {
 if (isset($_GET['code'])) {
   $client->authenticate($_GET['code']);
   $_SESSION['access_token'] = $client->getAccessToken();
-  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-  // $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
+  // header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+  $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
 }
 
 //Set Access Token to make Request
@@ -108,10 +108,8 @@ if ($client->getAccessToken()) {
   }
 } else {
   $auth_url = $client->createAuthUrl();
-  header('Location: ' . $auth_url);
-  exit;
-  // $responseArr["result"] = TRUE;
-  // $responseArr["url"] = filter_var($auth_url, FILTER_SANITIZE_URL);
+  $responseArr["result"] = TRUE;
+  $responseArr["url"] = filter_var($auth_url, FILTER_SANITIZE_URL);
 }
 ob_end_clean();
 exit(json_encode($responseArr,true));
