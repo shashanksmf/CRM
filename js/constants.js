6b4414ee86cecb703d68efb5c4b2d1ed86dcfd65
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-06-01 17:45:53 +0530

    Token Expiration while uploading bug solved

--- js/constants.js
+++ js/constants.js
@@ -2,8 +2,16 @@ var inspinia = angular.module('inspinia');
 inspinia.constant('crmconfig', {
 
   serverDomainName: 'https://upsailgroup.herokuapp.com',
+  // serverDomainName: 'http://localhost',
   defaultUserPic:'img/default-avatar.png',
   servicePath:'https://upsailgroup.herokuapp.com/Service',
   uploadFolder:'https://upsailgroup.herokuapp.com/uploads',
-  rootFilesUrl :'https://upsailgroup.herokuapp.com/files/Files/'	  
+  rootFilesUrl :'https://upsailgroup.herokuapp.com/files/Files/'
+  
+  // serverDomainName: 'https://upsailgroup.herokuapp.com',
+  // serverDomainName: 'http://localhost',
+  // defaultUserPic:'img/default-avatar.png',
+  // servicePath:'http://localhost:8080/CRM.git/trunk/Service',
+  // uploadFolder:'http://localhost:8080/CRM.git/trunk/uploads',
+  // rootFilesUrl :'http://localhost:8080/CRM.git/trunk/files/Files/'
 });
