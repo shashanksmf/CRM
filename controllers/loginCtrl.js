518d48bee090fc05e51bce1aec3326214277b2d8
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-06-01 12:08:00 +0530

    test

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -40,37 +40,40 @@ inspinia.controller('loginCtrl', ['$scope','$rootScope','$http','$q','API','$sta
 
 
 	$scope.googleLogIn = function(){
-		API.googleLogIn().then(function(response){
-			console.log(response);
-			if (response.data.result) {
-				window.location = response.data.url;
-			}
-			else if(response.data.token){
-	            localStorage.clear();
-				$rootScope.userEmail = response.data.userEmail;
-				$rootScope.userName = response.data.userName;
-				$rootScope.userId = response.data.userId;
-				$rootScope.token = response.data.token;
-				localStorage.setItem("userEmail",response.data.userEmail);
-				localStorage.setItem("userName",response.data.userName);
-				localStorage.setItem("userId",response.data.userId);
-				localStorage.setItem("token",response.data.token);
-				//localStorage.setItem("userUUID",response.data.responce);
-				$rootScope.userProfilePic = crmconfig.serverDomainName +"/"+ response.data.profilePic;
-				$scope.$emit('initialiseChat', { initChat : true });
+		window.location= baseHttpUrl+'/googleLogIn.php';
+		// API.googleLogIn().then(function(response){
+		// 	console.log(response);
+		// 	if (response.data.result) {
+		// 		window.location = response.data.url;
+		// 	}
+		// 	else if(response.data.token){
+	  //           localStorage.clear();
+		// 		$rootScope.userEmail = response.data.userEmail;
+		// 		$rootScope.userName = response.data.userName;
+		// 		$rootScope.userId = response.data.userId;
+		// 		$rootScope.token = response.data.token;
+		// 		localStorage.setItem("userEmail",response.data.userEmail);
+		// 		localStorage.setItem("userName",response.data.userName);
+		// 		localStorage.setItem("userId",response.data.userId);
+		// 		localStorage.setItem("token",response.data.token);
+		// 		//localStorage.setItem("userUUID",response.data.responce);
+		// 		$rootScope.userProfilePic = crmconfig.serverDomainName +"/"+ response.data.profilePic;
+		// 		$scope.$emit('initialiseChat', { initChat : true });
+		//
+		//
+		// 		// API.setAuth(response.data);
+		// 		$state.go("dashboards.home");
+		// 		// window.location = response.data.url;
+		// 	}
+		// 	else{
+		// 		$scope.error = response.data;
+		// 		$timeout(function(){
+		// 			$scope.error.responce = true;
+		// 		},10000)
+		// 	}
+		// });
 
 
-				// API.setAuth(response.data);
-				$state.go("dashboards.home");
-				// window.location = response.data.url;
-			}
-			else{
-				$scope.error = response.data;
-				$timeout(function(){
-					$scope.error.responce = true;
-				},10000)
-			}
-		});
 	}
 
 	$scope.facebookLogIn = function(){
