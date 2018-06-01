cf5f6b171e065f11862d4fa4a5a3e7ea4661ed44
LalitGadhari <35829744+LalitGadhari@users.noreply.github.com>
2018-06-01 12:41:16 +0530

    test

--- Service/afterGoogleLogIn.php
+++ Service/afterGoogleLogIn.php
@@ -33,26 +33,6 @@ $client->addScope("https://www.googleapis.com/auth/userinfo.email");
 //Send Client Request
 $objOAuthService = new Google_Service_Oauth2($client);
 
-//Logout
-if (isset($_REQUEST['logout'])) {
-  unset($_SESSION['access_token']);
-  $client->revokeToken();
-  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-  exit();
-
-  // $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
-}
-
-//Authenticate code from Google OAuth Flow
-//Add Access Token to Session
-if (isset($_GET['code'])) {
-  $client->authenticate($_GET['code']);
-  $_SESSION['access_token'] = $client->getAccessToken();
-  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-  exit();
-  // $responseArr["url"] = filter_var($redirect_uri, FILTER_SANITIZE_URL);
-}
-
 //Set Access Token to make Request
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
@@ -110,12 +90,6 @@ if ($client->getAccessToken()) {
     $token = $getNewtoken->getToken($addSocialUser['lastId']);
     $responseArr['token'] = $token['token'];
   }
-} else {
-  $auth_url = $client->createAuthUrl();
-  header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
-
-  // $responseArr["result"] = TRUE;
-  // $responseArr["url"] = filter_var($auth_url, FILTER_SANITIZE_URL);
 }
 ob_end_clean();
 exit(json_encode($responseArr,true));
