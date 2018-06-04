c6be5a2d4c8cafbb1b3ac6e9f739874b4ba0a9dd
shashank <shashanksmf@outlook.com>
2018-06-04 09:43:50 -0700

    test headers

--- Service/insertBulkData.php
+++ Service/insertBulkData.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Headers: Origin, token, Host,content-type,Content-Type');
-// header('Access-Control-Allow-Methods: POST, GET');
+header('Access-Control-Allow-Headers: Origin, token, Host, content-type, Content-Type');
+header('Access-Control-Allow-Methods: POST, GET');
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
