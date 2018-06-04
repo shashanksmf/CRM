7a629d02ee5255b0138b57b3d01ae20874447734
jaiswalaishwarya19 <39827198+jaiswalaishwarya19@users.noreply.github.com>
2018-06-04 16:47:19 +0530

    Options headers added

--- Service/insertBulkData.php
+++ Service/insertBulkData.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type,Origin, token, Host');
-header('Access-Control-Allow-Methods: POST, GET');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
