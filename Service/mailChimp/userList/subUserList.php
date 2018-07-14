9a7e509c0c283203476af5e95f2f527aec010dbc
jaiswalaishwarya19 <39827198+jaiswalaishwarya19@users.noreply.github.com>
2018-07-14 17:27:53 +0530

    comment removed

--- Service/mailChimp/userList/subUserList.php
+++ Service/mailChimp/userList/subUserList.php
@@ -1,32 +1,22 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-    require_once "mailChimpSubUserList.php";
-    require_once "../../../Controller/mailChimpConfig.php";
-    require_once "../../../Controller/mailChimpService.php";
+require_once "mailChimpSubUserList.php";
+require_once "../../../Controller/mailChimpConfig.php";
+require_once "../../../Controller/mailChimpService.php";
 
-	// class subUserList {
-
- //        public function subUserListFun() {
-
-            $mailChimpService = new MailChimpService();
-            $mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
-            $mailChimpApiKey = $mailChimpService->mailChimpApiKey = getenv("mailChimpApiKey");
-            $list_id = $mailChimpService->list_id = getenv('mailChimpListId');
-
-            $mailChimpSubUserList = new mailChimpSubUserList();
-            $result = $mailChimpSubUserList->userList($mailChimpApiKey,$mailChimpSubDomainInit,$list_id);
-            // $result = json_decode($result, true);
-            echo $result;
-
-    //     }
-
-    // }
+$mailChimpService = new MailChimpService();
+$mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
 
 
+$mailChimpApiKey = $mailChimpService->mailChimpApiKey = getenv("mailChimpApiKey");
+$list_id = $mailChimpService->list_id = getenv('mailChimpListId');
 
-
+$mailChimpSubUserList = new mailChimpSubUserList();
+$result = $mailChimpSubUserList->userList($mailChimpApiKey,$mailChimpSubDomainInit,$list_id);
+// $result = json_decode($result, true);
+echo $result;
 
 ?>
