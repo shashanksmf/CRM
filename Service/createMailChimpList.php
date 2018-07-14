db0fc26e899fdae43236b489dde6827f9fd6156f
jaiswalaishwarya19 <39827198+jaiswalaishwarya19@users.noreply.github.com>
2018-07-14 17:16:52 +0530

    create lest service with insert query

--- Service/createMailChimpList.php
+++ Service/createMailChimpList.php
@@ -1,74 +1,80 @@
 <?php
-// header("Access-Control-Allow-Origin: *");
-// header('Access-Control-Allow-Headers: Origin, token, Host');
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Origin, token, Host');
 
-// error_reporting(E_ALL);
-// ini_set('display_errors', '1');
-// require_once "./phpHeader/getHeader.php";
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once "./phpHeader/getHeader.php";
 
-// $headers = apache_request_headers();
-// require_once "./token/validateToken.php";
-
-// $userId = @$_GET['userId'];
-// $apiKey = @$_GET['apiKey'];
-// $listName = @$_GET['listName'];
-
+$headers = apache_request_headers();
+require_once "./token/validateToken.php";
 require_once "./mailChimp/list/createList.php";
 require_once ".../../../Controller/mailChimpConfig.php";
 require_once ".../../../Controller/mailChimpService.php";
+require_once "../Controller/StaticDBCon.php";
+$conn = new mysqli(StaticDBCon::$servername, StaticDBCon::$username, StaticDBCon::$password, StaticDBCon::$dbname);
+$responseArr = array();
+if($conn->connect_error) {
+	$responseArr["result"] = false;
+	$responseArr["reason"] = $conn->connect_error;
+	exit(json_encode($responseArr));
+}
+$userId=@$_GET['userId'];
+$apiKey = @$_GET['apiKey'];
+$name = @$_GET['name'];
+$type=@$_GET['type'];
+$company=@$_GET['company'];
+$address1=@$_GET['address1'];
+$address2=@$_GET['address2'];
+$city=@$_GET['city'];
+$state=@$_GET['state'];
+$zip=@$_GET['zip'];
+$country=@$_GET['country'];
+$phone=@$_GET['phone'];
+$permission_reminder=@$_GET['permission_reminder'];
+$from_name=@$_GET['from_name'];
+$from_email=@$_GET['from_email'];
+$subject=@$_GET['subject'];
+$language=@$_GET['language'];
+$email_type_option=@$_GET['email_type_option'];
+$date=date("Y-m-d");
 
-$mailChimpService = new MailChimpService();
-$mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
-$mailChimpApiKey = $mailChimpService->mailChimpApiKey = getenv("mailChimpApiKey");
-$list_id = $mailChimpService->list_id = getenv('mailChimpListId');
+  $mailChimpService = new MailChimpService();
+  $mailChimpSubDomainInit = MailChimpConfig::$mailChimpSubDomainInit;
+  $mailChimpApiKey = $mailChimpService->mailChimpApiKey = $apiKey;
+  $contactDetails=array('company'=>$company,'address1'=>$address1,'address2'=>$address2,'city'=>$city,
+  'state'=>$state,'zip'=>$zip,'country'=>$country,'phone'=>$phone);
+  $campaign_defaults=array('from_name'=>$from_name,
+  'from_email'=>$from_email,'subject'=>$subject,'language'=>$language
+  );
 
-$createList = new createList();
-$result = $createList->list($mailChimpApiKey,$mailChimpSubDomainInit);
-echo "createList",$result;
+  $createList = new createList();
+  $result = $createList->list($mailChimpApiKey,$name,$mailChimpSubDomainInit,$contactDetails,$permission_reminder,$campaign_defaults,$email_type_option);
+  $result = json_decode($result,true);
+  // print_r($result);
+    if (!array_key_exists('id', $result)) {
+        $resultArr['result'] = false;
+        $resultArr['reason'] = "list is invalid";
+        exit(json_encode($resultArr));
+  }
+   else{
+        $listid = $result["id"];
+	      // print_r($listid);
+        $insertListDetails="INSERT INTO mailchimp_createlist_details (listid,userId,APIKEY,listname,type,date_created,company,address1,address2,city,state,zip,country,phone,permission_reminder,from_name,from_email,subject,language,email_type_option,isActive)VALUES('".$listid."','".$userId."','".$mailChimpApiKey."','".$name."','".$type."','".$date."','".$company."','".$address1."','".$address2."','".$city."','".$state."','".$zip."','".$country."','".$phone."','".$permission_reminder."','".$from_name."','".$from_email."','".$subject."','".$language."','".$email_type_option."',1)";
+        if(mysqli_query($conn, $insertListDetails))
+       {
+          $resultArr['result']=true;
+          $resultArr['details']=$result;
+          exit(json_encode($resultArr));
+       }
+        else
+        {
+          $resultArr['result']=false;
+          $resultArr['reason']="cannot insert";
+          exit(json_encode($resultArr));
+         }
+      }
 
-// $result = json_decode($result, true);
-// $responseArr = array();
-// $responseArr = $result;
-
-// if ($responseArr['account_id'] != "") {
-//     $responseArr['result'] = true;
-//     // echo $responseArr;
-// }
-// else {
-//     $responseArr['result'] = false;
-//     exit $responseArr;
-// }
-
-// require_once "../Controller/StaticDBCon.php";
-// $conn = new mysqli(StaticDBCon::$servername, StaticDBCon::$username, StaticDBCon::$password, StaticDBCon::$dbname);
-// // Create connection
-
-// $responseArr = array();
-// if (!$conn) {
-// //echo "not conn";
-//     $responseArr["result"] = false;
-//     $responseArr["reason"] =  mysqli_connect_error();
-//     die($responseArr);
-// }
-// mysqli_set_charset($conn,"utf8");
-// $sql = "INSERT INTO .mailchimpdetails (userId,apiKey,listId,listName)
-// VALUES ('".$userId."','".$apiKey."','".$listId."','".$listName."')";
-
-// if (mysqli_query($conn, $sql)) {
-// //echo "if";
-//     $responseArr["result"] = true;
-//     $last_id = mysqli_insert_id($conn);
-//     $responseArr["lastId"] = $last_id;
-
-//     echo json_encode($responseArr);
-// } else {
-// //echo "else".mysqli_error($conn);
-//     $responseArr["result"] = false;
-//     $responseArr["reason"] = mysqli_error($conn);
-//     echo json_encode($responseArr);
-//    // echo "Error updating record: " . mysqli_error($conn);
-// }
-
-// mysqli_close($conn);
+ mysqli_close($conn);
 
 ?>
