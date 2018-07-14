28c931a8752a464156da65e2a92672b116fee619
jaiswalaishwarya19 <39827198+jaiswalaishwarya19@users.noreply.github.com>
2018-07-14 17:26:00 +0530

    comments removed and result sent in format req

--- Service/mailChimp/list/createList.php
+++ Service/mailChimp/list/createList.php
@@ -5,7 +5,18 @@
 
 class createList {
 
-  public function list($mailChimpApiKey,$mailChimpSubDomainInit) {
+  public function list($mailChimpApiKey,$name,$mailChimpSubDomainInit,$contactDetails,$permission_reminder,$campaign_defaults,$email_type_option) {
+
+    $data = array(
+        'apikey'        => $mailChimpApiKey,
+        'contact'  => $contactDetails,
+        'name'=> $name,
+        'email_type_option' => $email_type_option === 'true' ? true : false,
+        'permission_reminder'=>$permission_reminder,
+        'campaign_defaults'=>$campaign_defaults
+        );
+
+    $json_data = json_encode($data);
 
     $auth = base64_encode( 'user:'.$mailChimpApiKey);
     $ch = curl_init();
@@ -18,8 +29,9 @@ class createList {
     // curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
     $result = curl_exec($ch);
+// $result = json_decode($result, true);
     return $result;
   }
 }
