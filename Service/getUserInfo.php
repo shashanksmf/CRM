7f9eb267e5fe39aa183a8292d293c68002252a3e
jaiswalaishwarya19 <39827198+jaiswalaishwarya19@users.noreply.github.com>
2018-09-10 19:12:45 +0530

    user info headers removed

--- Service/getUserInfo.php
+++ Service/getUserInfo.php
@@ -1,12 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Headers: Origin, token, Host');
+// header('Access-Control-Allow-Headers: Origin, token, Host');
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-require_once "./phpHeader/getHeader.php";
-
-$headers = apache_request_headers();
+// require_once "./phpHeader/getHeader.php";
+// $headers = apache_request_headers();
 require_once "./token/validateToken.php";
 
 $responseArr = array();
